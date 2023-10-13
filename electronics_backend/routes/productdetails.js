2e4daa7793abc9275d743e5d3805ea2dd069d5f0
shivgupta07 <barsainya.shiv@gmail.com>
2023-10-13 23:00:59 +0530

    prdctdtls

--- electronics_backend/routes/productdetails.js
+++ electronics_backend/routes/productdetails.js
@@ -3,12 +3,15 @@ var router = express.Router();
 var pool=require('./pool')
 var upload=require('./multer')
 /* GET home page. */
-router.post('/submit_productdetails',upload.single('picture'), function(req, res, next) {
+router.post('/submit_productdetails',upload.any(), function(req, res) {
    try{
-    pool.query('insert into productdetails (categoryid, brandid, productid, productdetailsid, modelno, description, color, price, offerprice, stock, status, picture, hsncode) values(?,?,?,?,?,?,?,?,?,?,?,?,?)',[req.body.categoryid, req.body.brandid, req.body.productid, req.body.productdetailsid, req.body.modelno, req.body.description, req.body.color, req.body.price, req.body.offerprice, req.body.stock, req.body.status, req.body.file.filename, req.body.hsncode],function(error,result){
+    var filenames =req.files.map((file,index)=>file.filename)
+    pool.query('insert into productdetails (categoryid, brandid, productid, modelno, description, color, price, offerprice, stock, status, picture, hsncode) values(?,?,?,?,?,?,?,?,?,?,?,?)',
+    [req.body.categoryid, req.body.brandid, req.body.productid,  req.body.modelno, req.body.description, req.body.color, req.body.price, req.body.offerprice, req.body.stock, req.body.status, filenames+'', req.body.hsncode],function(error,result){
+       
         if(error)
-        {    
-            res.status(200).json({status:false,message:'Database error,pls contact database admin'})
+        {    console.log(error)
+            res.status(500).json({status:false,message:'Database error,pls contact database admin'})
         }
         else
         { 
@@ -22,5 +25,22 @@ catch(e)
 }
 });
 
+
+router.get('/display_all_products_details', function (req, res) {
+    try {
+        pool.query('select P.*, (select C.categoryname from category C where C.categoryid = P.categoryid) as categoryname, (select B.brandname from brands B where B.brandid = P.brandid) as brandname, (select Pr.productname from products Pr where Pr.productid = P.productid) as productname from productdetails P', function (error, result) {
+            if (error) {
+                res.status(200).json({ status: false, message: 'Database Error!' })
+            }
+            else {
+                res.status(200).json({ status: true, data: result })
+            }
+        })
+    }
+    catch (e) {
+        res.status(200).json({ status: false, message: 'Server Error!' })
+    }
+})
+ 
 
 module.exports = router;
